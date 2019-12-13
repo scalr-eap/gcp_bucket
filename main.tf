c6befcd043770243e17742ee0c8f11073b262450
Ryan <ryan.fee625@gmail.com>
2019-12-13 09:43:04 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "google" {
+    project = var.scalr_google_project
+    credentials = var.scalr_google_credentials
+    region      = var.region
+}
+
+resource "google_storage_bucket" "sample" {
+  name     = "my-test-bucket"
+  location = "US"
+}
